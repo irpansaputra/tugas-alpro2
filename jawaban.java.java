df1d298d70a5a219e162caf2ad0b313876d909c0
irpansaputra <irpansaputra720@gmail.com>
2022-11-09 18:22:23 +0700

    Menambah File

---
+++ jawaban.java.java
@@ -0,0 +1,8 @@
+public class jawaban {
+
+  public static void main(String[] args) {
+    String txt = "irpan saputra ilkom";
+    System.out.println(txt.toUpperCase());
+    System.out.println(txt.toLowerCase());
+  }
+}
